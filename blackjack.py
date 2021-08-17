b7f585a8d45be657c6de04a2abe9beeac1f18b1d
briansukalski <sukalskibrian@gmail.com>
2021-08-16 22:35:13 -0500

    Set up Game() class with constructor, define structure of play() method in comments

--- blackjack.py
+++ blackjack.py
@@ -26,7 +26,7 @@ class Deck():
             new_shuffle.append(cards_to_shuffle.pop(random.randint(0, len(cards_to_shuffle) - 1)))
         return new_shuffle
 
-    #Deals cards to all players in the game, including the dealer
+    #Deals cards to all players in the game, including the dealer, removing them from the top of the shuffled deck
     def deal_cards(self, num_players=1):
 
         player_hands = []
@@ -46,6 +46,29 @@ class Deck():
 
 test_deck = Deck()
 
+#Class that will actually be used to play the game of blackjack
+class Game():
+
+    def __init__(self, num_players=1, num_decks=1):
+        self.num_players = num_players
+        self.playing_deck = Deck(num_decks)
+        
+    def play(self):
+        #Collects chip buy-in from each player and allows them to name themselves if they wish
+
+        #At the beginning of each round, asks each player to make their bet; must be an integer, at least 1, and not more than the number of chips they have remaining
+
+        #Each player is dealt their cards, including the dealer. the dealer's second card is hidden from all of the players
+
+        #Each player gets their opportunity to decide what to do: hit or stay. If they go over 21, they bust and their turn is immediately over; otherwise, they continue until they decide to stay
+
+        #Dealer plays out their hand according to rules: must hit if value is under 17; otherwise, must stay
+
+        #Chip balances are adjusted for each player according to whether or not they beat the dealer in that round. In the event of a tie score (as long as the player has NOT busted), the player simply gets back the chips that they bet
+
+        #Each player is asked whether they would like to continue. Players who have been reduced to 0 chips have the option to buy back in with new chips, but their overall balance is still tracked. Players who elect to leave the game are removed after being told their total earnings/losses, and the next round is played with the remaining players
+
+        #The game continues until the last remaining player(s) elect to leave
 print(test_deck.shuffled_deck)
 
 print(test_deck.deal_cards(3))
